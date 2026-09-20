9ff557876453afa29bb23263f706e597672c13b4
Mechamorph <kazam0180@tuta.io>
2026-09-20 16:38:05 +0500

    init

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,13 +8,11 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=https://raw.githubusercontent.com/Nheko-Reborn/nheko/refs/heads/master/resources/nheko.svg
+export DESKTOP=/usr/share/applications/nheko.desktop
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
-
-# Additional changes can be done in between here
+quick-sharun /usr/bin/nheko /usr/lib/qt6/plugins/* /usr/lib/libKF6* /usr/share/color-schemes /usr/share/kstyle /usr/share/zsh
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
