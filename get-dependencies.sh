625524f03812c3473f5914bdf688cf5a8000366d
Mechamorph <118144070+kazam0180@users.noreply.github.com>
2026-09-20 16:19:29 +0500

    Initial commit

---
+++ get-dependencies.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -eu
+
+ARCH=$(uname -m)
+
+echo "Installing package dependencies..."
+echo "---------------------------------------------------------------"
+# pacman -Syu --noconfirm PACKAGESHERE
+
+echo "Installing debloated packages..."
+echo "---------------------------------------------------------------"
+get-debloated-pkgs --add-common --prefer-nano
+
+# Comment this out if you need an AUR package
+#make-aur-package PACKAGENAME
+
+# If the application needs to be manually built that has to be done down here
+
+# if you also have to make nightly releases check for DEVEL_RELEASE = 1
+#
+# if [ "${DEVEL_RELEASE-}" = 1 ]; then
+# 	nightly build steps
+# else
+# 	regular build steps
+# fi
+
+# Note that when building manually, you want to output the version of the
+# application to a ~/version file and remove VERSION from make-appimage.sh
